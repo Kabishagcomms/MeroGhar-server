cda86cb3ff8e551b0474c95eab3fa3d047a87e81
Kabishagcomms <np01cp4a220404@islingtoncollege.edu.np>
2024-12-27 22:42:37 +0545

    Added api route for user auth

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,26 +1,20 @@
+import { connect } from "mongoose";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+//return type of async function is promise
+// mongodb+srv://nick11444:${process.env.dbPassword}@fypnikan.bud3xcp.mongodb.net/test
 
-import {connect} from "mongoose"
-
-import * as dotenv from "dotenv"
-dotenv.config()
-    //return type of async function is promise 
-    // mongodb+srv://nick11444:${process.env.dbPassword}@fypnikan.bud3xcp.mongodb.net/test
-  
-const dbConnect=async()=>{
-    try{
-       // 
-         await connect(`mongodb+srv://nick11444:${process.env.dbPassword}@fypnikan.bud3xcp.mongodb.net/test`)
-         console.log("Database Connected")
-        
-         console.log("Next Called")
-          
-    }catch(e){
-        console.log(e);
-    }
-}
-
+const dbConnect = async () => {
+  try {
+    await connect(
+      `mongodb+srv://kabishag:${process.env.dbPassword}@meroghar.ah1tj.mongodb.net/?retryWrites=true&w=majority&appName=MeroGhar`
+    );
+    console.log("Database Connected");
+  } catch (e) {
+    const error = e as Error;
+    console.error("Database connection error:", error.message);
+  }
+};
 
 export default dbConnect;
-
-
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,20 +1,15 @@
 //this is index router where all the routes will be registered
 
 import { Router } from "express";
-import authRoutes from "../routes/auth/auth.routes"
-import userRoutes from "../routes/user/user.routes"
-import adminRoutes from "../routes/admin/admin.routes"  
-import propertyRoutes from "../routes/property/property.routes"
+import authRoutes from "../routes/auth/auth.routes";
+import userRoutes from "../routes/user/user.routes";
 
-const router=Router()
+const router = Router();
 
-//simply register the route here 
-//routes registration  before defning any routes 
+//simply register the route here
+//routes registration  before defning any routes
 //define prefix else nothing but the routepath should be uniqe
-router.use("/auth/v1",authRoutes);
-router.use("/user/v1",userRoutes)
-router.use("/admin/v1",adminRoutes)
-router.use("/property/v1",propertyRoutes)
+router.use("/auth/v1", authRoutes);
+router.use("/user/v1", userRoutes);
 
-
-export default router
+export default router;
